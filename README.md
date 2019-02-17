5f0a7afb6171e8af600c78bd72da3d2e6cd51039
yk1062008412 <yk1062008412@gmail.com>
2019-02-17 23:21:28 +0800

    connect database code.

---
+++ README.md
@@ -0,0 +1,9 @@
+该项目主要用于node进行数据库的增删改查操作。
+
+node + mysql
+
+# 安装mysql
+
+```
+npm install mysql
+```
---
+++ dbconnect.js
@@ -0,0 +1,20 @@
+// 引入mysql
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',       
+  user     : 'root',              
+  password : '',       
+  port: '3306',                   
+  database: 'node_project',
+  charset:'UTF8_GENERAL_CI'
+})
+
+connection.connect();
+
+connection.query('SELECT * FROM user_info', function(error, results, fields){
+  if (error) throw error;
+  console.log(results[0].phone_num);
+})
+
+connection.end();
